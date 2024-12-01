7d391ad5a00345b8483c4fc643c4413a1a00de3f
Kevin-Mansherov <kevinmansherov28@gmail.com>
2024-12-01 17:04:50 +0200

    Update user_logic.py

--- vacarion-system/src/logic/user_logic.py
+++ vacarion-system/src/logic/user_logic.py
@@ -1,8 +1,5 @@
-from utils.dal import DAL
-connect = DAL()
-
-
-countries = DAL.get_table("SELECT * FROM countries")
+from utils import dal
+countries = dal.DAL.get_table("SELECT * FROM countries")
 
 for country in countries:
     print(f"country name: {country["country_name"]}, country id: {
