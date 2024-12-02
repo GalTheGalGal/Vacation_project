5c820860c5f326aca0cd87458078d6455e0af28f
Kevin-Mansherov <kevinmansherov28@gmail.com>
2024-12-02 18:32:42 +0200

    fix

--- vacarion-system/src/logic/user_logic.py
+++ vacarion-system/src/logic/user_logic.py
@@ -1,8 +1,1 @@
-from src.utils.dal import DAL
 
-
-countries = DAL.get_table("SELECT * FROM countries")
-
-for country in countries:
-    print(f"country name: {country["country_name"]}, country id: {
-        country["country_id"]}")
