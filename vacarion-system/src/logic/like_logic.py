c7839f670413726c72c0e6cded8ec69fdb97a72f
Kevin-Mansherov <kevinmansherov28@gmail.com>
2024-12-02 22:28:40 +0200

    Update like_logic.py

--- vacarion-system/src/logic/like_logic.py
+++ vacarion-system/src/logic/like_logic.py
@@ -1,5 +1,15 @@
+<<<<<<< Updated upstream
 import sys
 sys.path.insert(1,r"C:\Users\kevin\OneDrive\שולחן העבודה\utils\main\vacarion-system\src\utils")
 import dal
 con = dal.DAL()
 
+=======
+from ..utils import dal
+con = dal.DAL()
+countries = con.get_table("select * from countries")
+
+for country in countries:
+    print(f"country id: {country["country_id"]}, vacation_title: {
+          country["vacation_title"]}")
+>>>>>>> Stashed changes
